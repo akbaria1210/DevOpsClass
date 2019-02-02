3716fc1b21d78a2e1b50e195d139112d6fa654f1
Your Name <you@example.com>
2019-02-02 12:45:37 -0600

    working yelp api call

---
+++ my_yelp.py
@@ -0,0 +1,21 @@
+import requests
+
+api_key = "lJITUJtIDXfqLXFgchBz_M115VP9BGs22YfT0fWbRg0nKCrQFF1XP_2oARLGQStqlzcdRqCzSYPpcDLm2hZEXoLlqzKhlwX7q--UA7b9r2nYS-YqJbJjJmCX69hVXHYx"
+
+url = "https://api.yelp.com/v3/businesses/search"
+
+my_headers = {
+    "Authorization": "Bearer %s" % api_key
+}
+
+my_params = {
+    "term": "restaurants",
+    "location": "chicago",
+    "limit": 3,
+}
+
+businesses_object = requests.get(url, headers=my_headers, params=my_params)
+
+businesses_dict = businesses_object.text
+
+print(businesses_dict)
